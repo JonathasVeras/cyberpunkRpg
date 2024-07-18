049b3b12057e7e228675e9dca552a63c6c1596f8
jonathas.veras.017 <jonathas.veras.017@ufrn.edu.br>
2024-07-17 22:37:08 -0300

    #feat: pontos passivos

--- js/scriptCriarFichas2.js
+++ js/scriptCriarFichas2.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     console.log("Script para criarFicha2.js carregado!");
 
-    document.getElementById("butSair").addEventListener("click", function() {
+    document.getElementById("butSair").addEventListener("click", function () {
         document.getElementById("formFicha").reset();
         window.location.href = 'fichasJogador.html';
     });
@@ -10,13 +10,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const butVoltar2 = document.getElementById("butVoltar2");
 
     if (butContinuar2) {
-        butContinuar2.addEventListener("click", function() {
+        butContinuar2.addEventListener("click", function () {
             saveFormDataToLocalStorage('criarFicha3.html');
         });
     }
 
     if (butVoltar2) {
-        butVoltar2.addEventListener("click", function() {
+        butVoltar2.addEventListener("click", function () {
             window.location.href = 'criarFicha1.html';
         });
     }
@@ -25,8 +25,45 @@ document.addEventListener("DOMContentLoaded", function() {
         const formData = captureFormData();
         let ficha = JSON.parse(localStorage.getItem('ficha')) || {};
         Object.assign(ficha, formData);
+
+        let atributos = {};
+        for (let key in ficha) {
+            if (key !== "nomePersonagem" && key !== "idadePersonagem" && key !== "sexoPersonagem" && key !== "antecedente" && key !== "atributos") {
+                atributos[key] = ficha[key];
+                delete ficha[key];
+            }
+        }
+
+        const vidaMaxima = parseInt(atributos.resistencia) + parseInt(atributos.forca);
+        const pontosVidaToken = vidaMaxima * 10;
+        const sanidadeMaxima = parseInt(atributos.autocontrole) + parseInt(atributos.sabedoria);
+        const percepcaoPassiva = (parseInt(atributos.destreza) + parseInt(atributos.sabedoria)) / 2;
+        const pesoMaximo = 4 + (parseInt(atributos.forca) + parseInt(atributos.resistencia)) * 5;
+        const identidade = (parseInt(atributos.humanidade) * 3) + 20
+        const velocidadeBase = (parseInt(atributos.agilidade) + parseInt(atributos.destreza)) / 2;
+        const movimento = parseInt(atributos.agilidade) / 2;
+        const correr = movimento * 4;
+        const caminharETerrenoDificil = movimento * 2;
+        const feridoESobrecarregado = movimento * 1;
+        const reducaoDanos = parseInt(atributos.resistencia) / 2
+
+        ficha.atributos = atributos;
+        ficha.vidaMaxima = vidaMaxima;
+        ficha.pontosVidaToken = pontosVidaToken;
+        ficha.sanidadeMaxima = sanidadeMaxima;
+        ficha.percepcaoPassiva = percepcaoPassiva;
+        ficha.pesoMaximo = pesoMaximo;
+        ficha.identidade = identidade;
+        ficha.velocidadeBase = velocidadeBase;
+        ficha.movimento = movimento;
+        ficha.correr = correr;
+        ficha.caminharETerrenoDificil = caminharETerrenoDificil;
+        ficha.feridoESobrecarregado = feridoESobrecarregado;
+        ficha.reducaoDanos = reducaoDanos;
+
+        console.log(ficha);
         localStorage.setItem('ficha', JSON.stringify(ficha));
-        window.location.href = nextPage;
+        // window.location.href = nextPage;
     }
 
     function captureFormData() {
@@ -36,9 +73,9 @@ document.addEventListener("DOMContentLoaded", function() {
         formGroups.forEach(form => {
             const atributo = form.querySelector('.atributos').textContent.trim();
             const inputs = form.querySelectorAll('input[type="text"]');
-            
+
             inputs.forEach(input => {
-                formData[input.name] = input.value; 
+                formData[input.name] = input.value;
             });
         });
 
